bd74fc86f564f0515f1296f2ea455686086230cd
Juan Carlos mamani huayta <carloshuayta112@gmail.com>
2020-04-12 16:53:04 -0400

    acomodanddo la ip del servidor del ing

--- src/Servicio/Conexion.java
+++ src/Servicio/Conexion.java
@@ -17,16 +17,11 @@ public class Conexion {
 
     private static Conexion instance = null;
 
-//    private final String db_name = "db_grupo09sc";//"laboratorios";
-//    private final String port = "5432";//"5454";
-//    private final String host = "mail.ficct.uagrm.edu.bo";//"127.0.0.1";
-//    private final String usuario = "grupo09sc";//"postgres";
-//    private final String password = "grupo09grupo09";//"tecnoweb";
     private final String db_name = "db_agenda";
-    private final String port = "5454";
-    private final String host = "127.0.0.1";
-    private final String usuario = "postgres";
-    private final String password = "tecnoweb";
+    private final String port = "5432";
+    private final String host = "mail.ficct.uagrm.edu.bo";
+    private final String usuario = "grupo09sc";
+    private final String password = "grupo09grupo09";
 
     private Connection conn;
 
